49968c040a77269fe2838473ea701a3405167f53
kttake <ktakemura@hks.harvard.edu>
2021-01-14 17:21:36 -0500

    tidy up future section

--- scripts/scatter.js
+++ scripts/scatter.js
@@ -1,17 +1,16 @@
 function drawScatterChart(cssSelector){
 
 var svg = dimple.newSvg(cssSelector, 590, 400);
-      d3.tsv("../data/launch_cost.tsv", function (data) {
-        var myChart = new dimple.chart(svg, data);
-        myChart.setBounds(60, 30, 505, 305)
-        var x = myChart.addCategoryAxis("x", "Year");
-        x.addOrderRule("Year");
-        myChart.addMeasureAxis("y", "Cost per Launch");
-        myChart.addSeries("Size", dimple.plot.bubble);
-        myChart.addLegend(180, 10, 360, 20, "right");
-        myChart.draw();
-      });
-    };
+  d3.tsv("../data/launch_cost.tsv", function (data) {
+    var myChart = new dimple.chart(svg, data);
+    myChart.setBounds(60, 30, 505, 305)
+    var x = myChart.addCategoryAxis("x", "Year");
+    x.addOrderRule("Year");
+    myChart.addMeasureAxis("y", "Cost per Launch");
+    myChart.addSeries("Size", dimple.plot.bubble);
+    myChart.addLegend(180, 10, 360, 20, "right");
+    myChart.draw();
+  });
+};
 
 drawScatterChart('#launch_cost');
-
